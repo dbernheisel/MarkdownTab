014e34e99974765cec0a015f1b9c67163a8193f7
minimalmichael <michaelpaguay@gmail.com>
2019-06-16 09:49:39 -0400

    styled the whole thing. got the js working. need to figure out saving

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 
-var converter = new showdown.Converter(),
-    text      = document.getElementById("inputtext").textContent,
-    html      = converter.makeHtml(text);
-
-
-document.getElementById('output').innerHTML = html;
-
 
 function convert() {
-  var converter = new showdown.Converter(),
-      text      = document.getElementById("inputtext").value,
-      html      = converter.makeHtml(text);
+          var converter = new showdown.Converter(),
+              text      = document.getElementById("inputtext").value,
+              html      = converter.makeHtml(text);
 
 
-  document.getElementById('output').innerHTML = html;
+          document.getElementById('markdowntext').innerHTML = html;
 }
+
+convert();
+
+function togglewindow(){
+  var elem = document.getElementById("togglebutton");
+  
+    if (elem.value==="<") {
+      elem.value = ">";
+      console.log("it works");
+    }
+    else elem.value = "<";
+
+
+  document.getElementById("input").classList.toggle("close");
+  document.getElementById("output").classList.toggle("expand");
+  document.getElementById("markdowntext").classList.toggle("expandtext");
+
+}
+
+window.addEventListener('keyup',function(e){
+    convert();
+},true);
