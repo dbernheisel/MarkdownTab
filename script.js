aaf811f6ec629186ab46c0669496c7bfdedd592c
minimalmichael <michaelpaguay@gmail.com>
2019-06-16 20:19:52 -0400

    got chrome saving through sync. starts with closed text area

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// FUNCTIONS THAT MAKE SHIT HAPPEN
 function convert() {
           var converter = new showdown.Converter(),
               text      = document.getElementById("inputtext").value,
@@ -9,14 +9,11 @@ function convert() {
           document.getElementById('markdowntext').innerHTML = html;
 }
 
-convert();
-
 function togglewindow(){
   var elem = document.getElementById("togglebutton");
-  
+
     if (elem.value==="<") {
       elem.value = ">";
-      console.log("it works");
     }
     else elem.value = "<";
 
@@ -24,9 +21,50 @@ function togglewindow(){
   document.getElementById("input").classList.toggle("close");
   document.getElementById("output").classList.toggle("expand");
   document.getElementById("markdowntext").classList.toggle("expandtext");
-
 }
 
+// LISTENERS THAT ARE WAITING FOR SOMETHING TO HAPPEN
+
 window.addEventListener('keyup',function(e){
-    convert();
+  convert();
 },true);
+
+
+document.getElementById("togglebutton").onclick = function(){
+  togglewindow();
+};
+
+window.addEventListener("load", function(e){
+  // GET TEXT FROM GOOGLE SYNC STORAGE
+  chrome.storage.sync.get(['key'], function(result) {
+    document.getElementById('inputtext').innerHTML = result.key;
+    convert();
+  });
+}, false);
+
+window.addEventListener("unload", function(e){
+  // SAVE TEXT ON GOOGLE SYNC STORAGE
+  var value = document.getElementById("inputtext").value;
+  chrome.storage.sync.set({key: value});
+}, false);
+
+// THIS FUNCTION SAVES TO GOOGLE EVERY SECOND
+var timer = null;
+window.addEventListener('keyup',function(e){
+  clearTimeout(timer);
+  timer = setTimeout(savetoGoogle, 1000)
+});
+
+function savetoGoogle() {
+  console.log("Calling google");
+  // SAVE TEXT ON GOOGLE SYNC STORAGE
+  var value = document.getElementById("inputtext").value;
+  chrome.storage.sync.set({key: value});
+  // GET TEXT FROM GOOGLE SYNC STORAGE
+  chrome.storage.sync.get(['key'], function(result) {
+    document.getElementById('inputtext').innerHTML = result.key;
+    convert();
+  });
+}
+
+togglewindow();
