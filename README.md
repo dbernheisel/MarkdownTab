8d19874fcbdbbafc00f0bb864258811b41ae9203
minimalmichael <michaelpaguay@gmail.com>
2019-06-21 17:43:45 -0400

    finalized extension. added all extra ads. published to chrome ext store

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# MarkdownTab
-A chrome extension with a better Markdown New Tab.
+# Hello MarkdownTab
+### A *Markdown* page that comes up __everytime__ we open a new tab
+
+ Useful for taking notes, keeping lists, saving links, or to be reminded of pictures or quotes.
+
+John Gruber and Aaron Swartz created the Markdown language in 2004. Its key design goal is readability. That the language be readable as-is, without looking like it has been marked up with tags or formatting instructions, unlike text formatted with a markup language, such as Rich Text Format (RTF) or HTML, which have obvious tags and formatting instructions. [More Info.](https://en.wikipedia.org/wiki/Markdown)
+
+### Example To Do List:
+- Look up how to make a Chrome Extension
+- Get supplies on [Amazon](http://www.amazon.com)
+- Find video on how to do the Floss Dance.
+
+You can learn how to use markdown [here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).
+
+> Special thanks to Devyn and Estevai for creating [Showdown](https://github.com/showdownjs/showdown) which translates markdown to html using javascript, and to [Mihir](https://github.com/plibither8) for inspiring the project.
+- Michael.
+
+---
+
+![Pokemon](https://media3.giphy.com/media/1342dTzTKIkbC0/giphy.gif?cid=790b76115d0d0a7872327a6e6307d86c&rid=giphy.gif)
--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 
 // FUNCTIONS THAT MAKE SHIT HAPPEN
 function convert() {
-          var converter = new showdown.Converter(),
-              text      = document.getElementById("inputtext").value,
-              html      = converter.makeHtml(text);
+  var converter = new showdown.Converter(),
+      text      = document.getElementById("inputtext").value,
+      html      = converter.makeHtml(text);
 
 
-          document.getElementById('markdowntext').innerHTML = html;
+  document.getElementById('markdowntext').innerHTML = html;
 }
 
 function togglewindow(){
   var elem = document.getElementById("togglebutton");
 
-    if (elem.value==="<") {
-      elem.value = "Edit";
-    }
-    else elem.value = "<";
-
+  if (elem.value==="<") { elem.value = "Edit"; }
+  else elem.value = "<";
 
   document.getElementById("input").classList.toggle("close");
   document.getElementById("output").classList.toggle("expand");
@@ -24,49 +21,53 @@ function togglewindow(){
 }
 
 // LISTENERS THAT ARE WAITING FOR SOMETHING TO HAPPEN
-
-window.addEventListener('keyup',function(e){
-  convert();
-},true);
-
-
 document.getElementById("togglebutton").onclick = function(){
   togglewindow();
 };
 
+window.addEventListener('keyup',function(e){ convert(); },true);
+
 window.addEventListener("load", function(e){
   // GET TEXT FROM GOOGLE SYNC STORAGE
-
   chrome.storage.sync.get(['key'], function(result) {
     if(result.key != undefined){
       document.getElementById('inputtext').innerHTML = result.key;
       convert();
     }
     else {
-      var text = `
-# Hello MarkdownTab
-## This is a subtitle
+      var text = `# Hello Markdown Tab
+### A *Markdown* page that comes up __everytime__ we open a new tab
 
-Markdown is a lightweight markup language with plain text formatting syntax. John Gruber created the Markdown language in 2004 in collaboration with Aaron Swartz on the syntax. Its key design goal is readability – that the language be readable as-is, without looking like it has been marked up with tags or formatting instructions, unlike text formatted with a markup language, such as Rich Text Format (RTF) or HTML, which have obvious tags and formatting instructions. [More Info.](https://en.wikipedia.org/wiki/Markdown)
+Useful for taking notes, keeping lists, saving links, or to be reminded of pictures or quotes.
 
-- something
-- something else
-- something nice
+John Gruber and Aaron Swartz created the Markdown language in 2004. Its key design goal is readability. That the language be readable as-is, without looking like it has been marked up with tags or formatting instructions, unlike text formatted with a markup language, such as Rich Text Format (RTF) or HTML, which have obvious tags and formatting instructions. [More Info.](https://en.wikipedia.org/wiki/Markdown)
 
-You can learn how to use markdown at [this](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) link.
+### Example To Do List:
+- Look up how to make a Chrome Extension
+- Get supplies on [Amazon](http://www.amazon.com)
+- Find video on how to do the Floss Dance.
+
+You can learn how to use markdown [here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet), and edit this page by clicking "Edit" on the top left.
+
+> Special thanks to Devyn and Estevai for creating [Showdown](https://github.com/showdownjs/showdown) which translates markdown to html using javascript, and to [Mihir](https://github.com/plibither8) for inspiring the project. Colors were grabbed from [Atom](https://atom.io/) code editor.
+- Michael.
 
 ---
 
-![Fight Club](https://media3.giphy.com/media/YPF8N1ARFOE6c/giphy.gif?cid=790b76115d06317530676d326bcae53d&rid=giphy.gif)
-      `;
+![Pokemon](https://media3.giphy.com/media/1342dTzTKIkbC0/giphy.gif?cid=790b76115d0d0a7872327a6e6307d86c&rid=giphy.gif)`;
       document.getElementById('inputtext').innerHTML = text;
       convert();
     }
   });
 }, false);
 
+
+// ---
+// GOOGLE SYNC SAVE
+// ---
+
+// SAVE TEXT ON GOOGLE SYNC STORAGE
 window.addEventListener("unload", function(e){
-  // SAVE TEXT ON GOOGLE SYNC STORAGE
   var value = document.getElementById("inputtext").value;
   chrome.storage.sync.set({key: value});
 }, false);
